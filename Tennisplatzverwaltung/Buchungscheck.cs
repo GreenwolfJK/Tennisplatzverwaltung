6701a67900b88da42f09ec0aa029969ec91a86cd
Sebastian <anrsirprim@gmail.com>
2015-06-26 08:33:35 +0200

    Ihr Ferkel

---
+++ Tennisplatzverwaltung/Buchungscheck.cs
@@ -0,0 +1,74 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tennisplatzverwaltung
+{
+    class Buchungscheck
+    {
+        int platznr;
+        DateTime wunschtermin;
+        Time start;
+        Time ende;
+        String playerName;
+
+        DBConnect db = new DBConnect();
+
+        public Buchungscheck()
+        {
+            this.platznr = 1;
+            this.wunschtermin = DateTime.Today;
+            this.start = new Time(11, 15);
+            this.ende = new Time(12, 30);
+            this.playerName = "Erna Engel";
+        }
+
+        public Buchungscheck(int _platznr, DateTime _wunschtermin, Time _start, Time _ende, String _playerName)
+        {
+            this.platznr = _platznr;
+            this.wunschtermin = _wunschtermin;
+            this.start = _start;
+            this.ende = _ende;
+            this.playerName = _playerName;
+        }
+
+        public bool checkBuchung()
+        {
+            bool dbopen = db.openDatabase();
+            //SqlCommand command = new SqlCommand("SELECT * from buchungen where datum_start = wunschtermin", db.connection); 
+            //DataTable tbl = db.fillTable("SELECT datum_start, datum_end from buchungen where datum_start = wunschtermin and platz_id = platznr");
+            MySqlDataReader reader = db.executeQuery("SELECT datum_start, datum_end FROM buchungen WHERE platz_id = " + platznr);
+            // get data from database
+            if (reader.HasRows)
+            {
+                Zeitspanne check = new Zeitspanne(this.start, this.ende);
+
+                while (reader.Read())
+                {
+                    //Console.WriteLine("{0}\t{1}", reader.GetInt32(0),
+                    //    reader.GetString(1));
+                    Time DataStart = new Time(reader.GetDateTime(0).Hour, reader.GetDateTime(0).Minute);
+                    Time DataEnd = new Time(reader.GetDateTime(1).Hour, reader.GetDateTime(1).Minute);
+                    Zeitspanne check2 = new Zeitspanne(DataStart, DataEnd);
+
+                    if (check.checkOverlap(check2))
+                    {
+                        MessageBox.Show("Es liegt eine Überschneidung vor");
+                        return false;
+                    }
+                }
+            }
+
+            // iterate over data
+            // check each piece of data for overlap
+            MessageBox.Show("Es liegt keine Überschneidung vor");
+            return true;
+        }
+    }
+}
---
+++ Tennisplatzverwaltung/Time.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tennisplatzverwaltung
+{
+    class Time
+    {
+        int hour;
+        int minute;
+
+        public Time()
+        {
+            this.hour = 0;
+            this.minute = 0;
+        }
+
+        public Time(int h, int m)
+        {
+            this.hour = h % 24;
+            this.minute = m % 60;
+        }
+
+        public bool earlierThan(Time x)
+        {
+            if (this.hour < x.hour)
+            {
+                return true;
+            }
+            else if (this.hour == x.hour && this.minute < x.minute)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool laterThan(Time x)
+        {
+            if (this.hour > x.hour)
+            {
+                return true;
+            }
+            else if (this.hour == x.hour && this.minute > x.minute)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool ealierOrEqual(Time x)
+        {
+            return !this.laterThan(x);
+        }
+
+        public bool laterOrEqual(Time x)
+        {
+            return !this.earlierThan(x);
+        }
+    }
+}
